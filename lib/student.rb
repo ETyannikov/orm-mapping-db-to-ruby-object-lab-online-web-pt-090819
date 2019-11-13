ddc62b5031b775f08ff7e3016b065a00346f1745
Eddy Tyannikov <etyannikov@gmail.com>
2019-11-13 23:43:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,11 @@ class Student
     end
   end
   
-  def self.al
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 9
+    SQL
+  end
 
   def self.find_by_name(name)
     sql = <<-SQL
